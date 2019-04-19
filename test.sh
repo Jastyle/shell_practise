79ccfef384b7982b17c9dae409357547d69c8798
Jastyle <woshigebai@gmail.com>
2019-04-19 07:26:24 +0000

    push pop dir in _DIR_STACK

--- test.sh
+++ test.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-name="testsh"
 echo $name
-cat test.sh | while read line
-do
-	echo "${line}"
-done
-echo "$IFS"
+#cat test.sh | while read line
+#do
+#	echo "${line}"
+#done
+#echo "$IFS"
 PATH=
 SetPath() {
 	PATH=${PATH=:"/sbin:/bin"}
 	for _dir in $@;do
 		if [ -d "$_dir" ]
-			then
-				PATH="$PATH:$_dir"
+		then
+			PATH="$PATH:$_dir"
 		fi
 	done
 	echo "$PATH"
@@ -32,7 +31,6 @@ dirs() {
 	IFS="$OLDIFS"
 		
 }
-dir /bin:/usr
 
 pushd() {
 	REQ=${1:=.}
@@ -42,6 +40,7 @@ pushd() {
 		if [ $? -eq 0 ]
 		then
 			_DIR_STACK="`pwd`:$_DIR_STACK"
+			export _DIR_STACK
 			dirs
 		else
 			echo " ERROR:cannot change to directory $REQ" >&2
@@ -53,5 +52,69 @@ pushd() {
 
 			
 }
+
+_popd_helper() {
+	POPD="$1"
+	if [ -z "$POPD" ]
+	then
+		echo " ERROR: $POPD is not a directory"
+	fi
+	shift 1
+	if [ -n "$1" ]
+	then
+		_DIR_STACK="$1"
+		shift
+		for i in "$@";
+		do
+			_DIR_STACK="$_DIR_STACK:$1"	
+		done
+	else
+		_DIR_STACK=
+	fi
+	if [ -n "$POPD" ]
+	then
+		echo "value $POPD"
+		cd "$POPD"
+		if [ $? -ne 0 ]
+		then
+			echo " ERROR: cannot cd to ${POPD}" >&2 
+		else
+			pwd
+		fi
+	else
+		echo "ERROR: ${POPD} is not a directory" >&2
+	fi
+	unset POPD
+	export _DIR_STACK
+
+
+}
+
+popd() {
+	OLDIFS="$IFS"
+	IFS=:
+	_popd_helper $_DIR_STACK
+	IFS="$OLDIFS"
+
+}
+
+
+pushd /home/zhangwanquan/env
+echo "after push $_DIR_STACK"
+popd
+echo "after opop $_DIR_STACK"
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
